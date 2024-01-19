60250bb6eac5794ef0d8309f11d9640845a38d65
Gareth King <sluglit@gmail.com>
2024-01-19 09:37:56 +0200

    Initial Deployment

--- download-models.ps1
+++ download-models.ps1
@@ -1,13 +1,26 @@
+Echo "@see https://localai.io/models/"
+Echo "@see https://github.com/go-skynet/model-gallery"
+
+
+Echo "`nDownloading gpt-3.5-turbo"
 Invoke-WebRequest -Uri http://localhost:8080/models/apply `
     -Method POST `
     -ContentType 'application/json; charset=utf-8' `
     -Body '{"id": "model-gallery@gpt-3.5-turbo"}'
 
+Echo "`nDownloading text-embedding-ada-002"
 Invoke-WebRequest -Uri http://localhost:8080/models/apply `
     -Method POST `
     -ContentType 'application/json; charset=utf-8' `
     -Body '{"id": "model-gallery@text-embedding-ada-002"}'
 
+Echo "`nDownloading thebloke__mistral-7b-instruct-v0.1-gguf__mistral-7b-instruct-v0.1.q4_k_m.gguf"
+Invoke-WebRequest -Uri http://localhost:8080/models/apply `
+    -Method POST `
+    -ContentType 'application/json; charset=utf-8' `
+    -Body '{"id": "huggingface@thebloke__mistral-7b-instruct-v0.1-gguf__mistral-7b-instruct-v0.1.q4_k_m.gguf"}'
+
+Echo "`nDownloading mistral"
 Invoke-WebRequest -Uri http://localhost:8080/models/apply `
     -Method POST `
     -ContentType 'application/json; charset=utf-8' `
